d6b8c7aaa0bbb7ef7b3eabf52cbdf62e32fa09b9
Kevin Deyarmin <kdeyarmin@comcast.net>
2025-12-28 22:29:38 +0000

    File changes

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,6 +1,7 @@
+
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
-import { cn } from "@/lib/utils"
+import { cn } from "@/utils" // Changed from "@/lib/utils"
 
 const Label = React.forwardRef(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
