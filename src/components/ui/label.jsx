51c8e0c941f2433865b4276bcc1899cdcb0f49ec
Kevin Deyarmin <kdeyarmin@comcast.net>
2025-12-28 22:40:47 +0000

    File changes

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,23 +1,26 @@
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+function cn(...inputs) {
+  return twMerge(clsx(inputs));
 }
 
-const Label = React.forwardRef(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(
-      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-      className
-    )}
-    {...props}
-  />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+const Label = React.forwardRef((props, ref) => {
+  const { className, ...otherProps } = props;
+  return (
+    <LabelPrimitive.Root
+      ref={ref}
+      className={cn(
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        className
+      )}
+      {...otherProps}
+    />
+  );
+});
 
-export { Label }
-export default Label
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };
