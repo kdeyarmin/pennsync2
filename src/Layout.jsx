11a0554d4fdd95a695d63a1ddd2fa12493fb05fb
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-01-06 20:28:30 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -603,7 +603,7 @@ export default function Layout({ children, currentPageName }) {
       )}
 
       {/* Main Content */}
-      <main className="flex-1 lg:overflow-auto pt-16 lg:pt-0 pb-24 lg:pb-0 bg-gray-50 min-h-screen">{children}</main>
+      <main className="flex-1 lg:overflow-auto pt-16 lg:pt-0 pb-24 lg:pb-0 bg-gradient-to-br from-gray-50 to-gray-100 min-h-screen">{children}</main>
 
       {/* Offline Indicator */}
       <OfflineIndicator />
