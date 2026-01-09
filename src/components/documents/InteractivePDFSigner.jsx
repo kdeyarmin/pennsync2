fef18c1ad7eb3467a70cfd7442da7fab07db884e
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-01-09 21:40:31 +0000

    File changes

--- src/components/documents/InteractivePDFSigner.jsx
+++ src/components/documents/InteractivePDFSigner.jsx
@@ -23,7 +23,9 @@ import { toast } from "sonner";
 import * as pdfjsLib from "pdfjs-dist";
 
 // Configure PDF.js worker
-pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+if (typeof window !== 'undefined') {
+  pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+}
 
 export default function InteractivePDFSigner({ 
   pdfUrl, 
