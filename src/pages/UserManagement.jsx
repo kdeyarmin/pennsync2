879b81d09cedeb5d320f6601da27528408b81716
Kevin Deyarmin <kdeyarmin@comcast.net>
2026-03-06 16:56:50 +0000

    File changes

--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -773,6 +773,7 @@ export default function UserManagement() {
                           </TableCell>
                         </TableRow>
                       )}
+                      </React.Fragment>
                     );
                   })}
                 </TableBody>
